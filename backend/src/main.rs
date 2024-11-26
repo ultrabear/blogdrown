c98181baec012cbcfd7bc1fa15b954f5d10b17c1
ultrabear <bearodark@gmail.com>
2024-11-26 02:07:54 -0800

    add latency simulating middleware for development

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code)]
 
 use core::error;
-use std::{env, sync::Arc};
+use std::{env, sync::Arc, time::Duration};
 
 use hmac::{Hmac, Mac};
 use sha2::Sha384;
@@ -25,10 +25,16 @@ struct BlogDrownState {
     prisma: Arc<PrismaClient>,
     jwt_secret: Hmac<Sha384>,
     production: bool,
+    sim_latency: Option<Duration>,
 }
 
-use axum::http::Method;
 use axum::Router;
+use axum::{
+    extract::{Request, State},
+    http::Method,
+    middleware::Next,
+    response::Response,
+};
 use logger::response_logger;
 use prisma::PrismaClient;
 
@@ -43,6 +49,26 @@ fn serve_frontend() -> ServeDir<SetStatus<ServeFile>> {
                 .precompressed_gzip()
                 .precompressed_br(),
         )
+}
+
+async fn sim_latency(State(state): State<BlogDrownState>, req: Request, next: Next) -> Response {
+    if state.production {
+        return next.run(req).await;
+    }
+
+    if let Some(d) = state.sim_latency {
+        tracing::debug!("simulating request latency: {d:?}");
+        tokio::time::sleep(d).await;
+
+        let res = next.run(req).await;
+
+        tracing::debug!("simulating response latency: {d:?}");
+        tokio::time::sleep(d).await;
+
+        return res;
+    } else {
+        return next.run(req).await;
+    }
 }
 
 #[tokio::main]
@@ -60,10 +86,21 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
         )?,
         production: env::var("BLOGDROWN_DEV")
             .map_or(true, |s| !matches!(s.to_lowercase().as_str(), "1" | "true")),
+        sim_latency: env::var("BLOGDROWN_LATENCY")
+            .ok()
+            .and_then(|s| s.parse().ok())
+            .map(Duration::from_millis),
     };
 
     if !state.production {
         tracing::warn!("running in development mode because BLOGDROWN_DEV was set");
+
+        if let Some(d) = state.sim_latency {
+            tracing::warn!(
+                "running with simulated latency: {d:?} (inbound and outbound, RTT {:?})",
+                d * 2
+            );
+        }
     } else {
         tracing::info!("running in production mode");
     }
@@ -83,14 +120,15 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
     let routes = Router::new()
         .nest("/api", api::api_routes())
         .fallback_service(serve_frontend())
-        .with_state(state)
+        .with_state(state.clone())
         .layer(
             CorsLayer::new()
                 .allow_methods([Method::GET, Method::POST, Method::PUT])
                 .allow_origin(Any),
         )
         .layer(TraceLayer::new_for_http())
-        .layer(axum::middleware::from_fn(response_logger));
+        .layer(axum::middleware::from_fn(response_logger))
+        .layer(axum::middleware::from_fn_with_state(state, sim_latency));
 
     axum::serve(listener, routes).await?;
 
